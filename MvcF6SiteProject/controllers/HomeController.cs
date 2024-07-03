e00c72fbf5aca58811a321a5a27769f13b67333b
Juniper Luck <luckn1891@gmail.com>
2024-07-03 17:14:55 +0100

    Initial Commit
    Initial commit and repository creation.
    
    ASP.NET MVC and Foundation 6 framework set up.

---
+++ MvcF6SiteProject/controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcF6SiteProject.controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
